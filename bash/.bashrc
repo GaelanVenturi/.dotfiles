4b25ef1cfd686722844ab2da6e65893d2a0ce509
Gaelan Venturi <gaelanv@pm.me>
2023-09-26 12:36:34 -0400

    Added conda initialization to .bashrc so environment loads everytime

--- bash/.bashrc
+++ bash/.bashrc
@@ -8,5 +8,7 @@
 alias ls='ls --color=auto'
 PS1='[\u@\h \W]\$ '
 
+[ -f /opt/anaconda/etc/profile.d/conda.sh ] && source /opt/anaconda/etc/profile.d/conda.sh
+
 eval "$(starship init bash)"
 exec fish
--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -9,6 +9,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "plugins" },
   },
   defaults = {
