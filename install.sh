dd648e59d30007acfb78d465292d37b9e58d12e0
Gaelan Venturi <gaelanv@pm.me>
2023-09-29 20:30:48 -0400

    Added links for p10k and ohmyzsh

--- install.sh
+++ install.sh
@@ -39,7 +39,6 @@ chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chmod 744 ~/.oh-my-zsh/oh-my-zsh.sh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
 # Install oh-my-zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
