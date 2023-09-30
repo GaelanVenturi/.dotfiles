b9189a4ec7c1d1aeaedce4e0bb012f11a2274d17
Gaelan Venturi <gaelanv@pm.me>
2023-09-29 20:27:29 -0400

    Added permissions of oh-my-zsh folder

--- install.sh
+++ install.sh
@@ -36,8 +36,8 @@ paru -S zsh zsh-completions
 
 chsh -s /bin/zsh
 
-# Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chmod 744 ~/.oh-my-zsh/oh-my-zsh.sh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Install oh-my-zsh plugins
