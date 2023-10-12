38bd7bb609b7ae0e6aef35372c0a5aa0992049a5
Gaelan Venturi <gaelanv@pm.me>
2023-10-12 08:00:02 -0400

    added ctrl+backspace to delete word to neovim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Map Ctrl+Backspace to delete word backwards
+-- Note: for this to work with neovim in a windows terminal add the following
+-- code to your `settings.json` under "actions"
+-- {
+--           "command": {
+--               "action": "sendInput",
+--               "input": "\u0017"
+--           },
+--           "keys": "ctrl+backspace"
+--       }
+vim.api.nvim_set_keymap("i", "<C-BS>", "<C-w>", { noremap = true, silent = true })
