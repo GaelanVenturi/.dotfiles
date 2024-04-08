89f32283dcaf686fabb7fceb14969ceb52446a16
Gaelan Venturi <gaelanv@pm.me>
2024-04-08 07:40:58 -0400

    Updated setup.sh - Now removes conflicts for nvim and others - Specifically targets and replaces bashrc files with .dotfiles version

--- setup.sh
+++ setup.sh
@@ -20,6 +20,44 @@ configonly=(
 	nvim
 )
 
+# Explicitly remove known conflicting files
+remove_known_conflicts() {
+	local usr=$1
+	# Define a list of known conflicting files for each app
+	declare -A known_conflicts
+	known_conflicts[bash]=".bash_profile .bashrc"
+	known_conflicts[git]=".gitconfig"
+
+	for app in "${!known_conflicts[@]}"; do
+		for file in ${known_conflicts[$app]}; do
+			if [[ -f "${usr}/${file}" || -L "${usr}/${file}" ]]; then
+				echo "Removing known conflicting file: ${usr}/${file}"
+				rm -f "${usr}/${file}"
+			fi
+		done
+	done
+
+}
+
+# resolve stow conflicts by removing or backing up conflicting files
+resolve_conflicts() {
+	usr=$1
+	app=$2
+	target=${usr}/$3 # target path, if it's for config, it'll be passed as an additional argument
+	echo "Resolving conflicts for ${app} in ${target}"
+
+	# Perform a dry run to find conflicts
+	conflicts=$(stow -n -v -t "${target}" "${app}" 2>&1 | grep "existing target is neither a link nor a directory" | awk '{print $NF}')
+
+	# If there are conflicts, remove the conflicting files
+	for conflict in $conflicts; do
+
+		echo "Removing conflicting file: ${target}/${conflict}"
+		rm -rf "${target}/${conflict}"
+	done
+
+}
+
 # run the stow command for the passed in directory ($2) in location $1
 stowit() {
 	usr=$1
@@ -37,11 +75,15 @@ stowConfig() {
 	target="${usr}/.config/${app}"
 	mkdir -p "${target}/${app}"
 	echo "Stowing ${app} into ${target} for user ${usr}"
+
+	resolve_conflicts "${usr}" "${app}" ".config/${app}"
 	stow -v -R -t "${target}" "${app}"
 }
-
 echo ""
 echo "Stowing apps for user: ${whoami}"
+
+# Remove known conflicting files before starting
+remove_known_conflicts "${HOME}"
 
 # install apps available to local users and root
 for app in ${base[@]}; do
